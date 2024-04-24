2792688f8531ed44f2125357b411890291a5de81
Camilobarre <barrecami@gmail.com>
2024-04-24 11:26:48 -0500

    Se crea la semántica de la clase

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+console.log("Creacion de Arrays");
+const array1= new Array;
+const array2= Array("a", "b", "c")
+const array3=[]
+
+console.log(array1);
+console.log(array2);
+console.log(array3);
+
+console.groupEnd()
